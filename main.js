3112e64beeb3971183f9799f1e55c2a4cc1fb464
D-Vasylchenko <145137182+D-Vasylchenko@users.noreply.github.com>
2024-03-19 00:52:27 +0200

    added background created class 'Sprite'

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 
-canvas.width = 1024;
-canvas.height = 576;
+canvas.width = 1900;
+canvas.height = 900;
+const scaledCanvas  = {
+    width: canvas.width / 4,
+    height: canvas.height /4
+}
+
+class Sprite {
+    constructor({position, imageSrc}) {
+        this.position = position;
+        this.image = new Image();
+        this.image.src = imageSrc;
+    }
+
+    draw() {
+        if (!this.image) return
+        c.drawImage(this.image, this.position.x, this.position.y);
+    }
+
+    update() {
+        this.draw()
+    }
+}
 
 class Player {
     constructor() {
@@ -15,12 +36,13 @@ class Player {
             y: 0,
         }
     }
-    draw(){
+
+    draw() {
         c.fillStyle = 'red';
         c.fillRect(this.position.x, this.position.y, 40, 40);
     }
 
-    update(){
+    update() {
         this.draw();
         this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
@@ -43,10 +65,25 @@ const keys = {
         pressed: false,
     },
 }
+const background = new Sprite({
+        position: {
+            x: 0,
+            y: 0,
+        },
+        imageSrc: './img/background.png'
+    }
+)
+
 function animate() {
     window.requestAnimationFrame(animate);
     c.fillStyle = 'black';
     c.fillRect(0, 0, canvas.width, canvas.height);
+
+    c.save();
+    c.scale(4,4);
+    c.translate(0, -background.image.height + scaledCanvas.height);
+    background.update();
+    c.restore();
     player.update();
     player.velocity.x = 0;
     player.velocity.y = 0;
@@ -55,11 +92,12 @@ function animate() {
     if (keys.w.pressed) player.velocity.y = -1;
     if (keys.s.pressed) player.velocity.y = 1;
 }
+
 animate();
 
 window.addEventListener('keydown', (event) => {
     console.log(event);
-    switch (event.keyCode){
+    switch (event.keyCode) {
         case 68:
             keys.d.pressed = true;
             break;
@@ -76,7 +114,7 @@ window.addEventListener('keydown', (event) => {
 });
 window.addEventListener('keyup', (event) => {
     console.log(event);
-    switch (event.keyCode){
+    switch (event.keyCode) {
         case 68:
             keys.d.pressed = false;
             break;
